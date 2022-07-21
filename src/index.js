730855272e3fa2eb2b97629fd4bd383f8f8f5c09
Roger Zhang <1787009521@qq.com>
2022-07-21 09:54:44 +0800

    feat: 支持IE

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-const str = "hi"
-console.log(str)
+import { a } from "./a.js";
+const b = import('./b.js')
+
+const hi = () => {
+    console.log(a)
+    console.log(b)
+    console.log(Promise.resolve('test promise'))
+}
+
+hi()
